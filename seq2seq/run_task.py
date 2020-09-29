65b82d4ad3d2179bda510ee95d0b92deddcf2c9b
Jurik Juraska <juraska.juraj@gmail.com>
2020-09-28 17:21:39 -0700

    Enables metric evaluation for reranked and non-reranked predictions in a single run.

--- seq2seq/run_task.py
+++ seq2seq/run_task.py
@@ -388,27 +388,12 @@ def test(config, dataset_class, device='cpu'):
 
         predictions.append(outputs_decoded)
 
-    if config.semantic_reranking:
-        predictions = rerank_beams(predictions, test_set.get_mrs_as_dicts())
-    predictions = [pred_beam[0] for pred_beam in predictions]
-
+    # Make sure the output directory exists for the given dataset
     predictions_dir = os.path.join('seq2seq', 'predictions', test_set.name)
     if not os.path.exists(predictions_dir):
         os.makedirs(predictions_dir)
 
-    # Save generated utterances along with their corresponding MRs into a CSV file
-    file_name_root = compose_output_file_name(config)
-    file_name = f'{file_name_root}.csv'
-    df_predictions = pd.DataFrame({'mr': test_set.get_mrs(raw=True), 'utt': predictions})
-    df_predictions.to_csv(os.path.join(predictions_dir, file_name), index=False, encoding='utf-8-sig')
-
-    # Save generated utterances in a text file (for reference-based metric evaluation)
-    file_name = f'{file_name_root}_utt_only.txt'
-    predictions_file = os.path.join(predictions_dir, file_name)
-    with open(predictions_file, 'w') as f_out:
-        for prediction in predictions:
-            f_out.write(prediction + '\n')
-
+    # Prepare the metrics script command, and create the reference file for the given dataset
     eval_dir = os.path.join('seq2seq', 'eval')
     metrics_script = 'python ' + os.path.join(eval_dir, 'E2E', 'measure_scores.py')
     reference_file = os.path.join(eval_dir, 'test_references_{}.txt'.format(test_set.name))
@@ -416,24 +401,50 @@ def test(config, dataset_class, device='cpu'):
         print('>> Generating a reference file for the "{}" test set.'.format(test_set.name))
         test_set.create_reference_file_for_testing()
 
-    # Run the metrics script provided by the E2E NLG Challenge
-    os.system(metrics_script + ' ' + reference_file + ' ' + predictions_file)
+    eval_configurations = []
+
+    if config.semantic_reranking:
+        predictions_reranked = rerank_beams(predictions, test_set.get_mrs_as_dicts())
+        predictions_reranked = [pred_beam[0] for pred_beam in predictions_reranked]
+        eval_configurations.append((predictions_reranked, True))
+
+    # For the evaluation of non-reranked predictions select the top candidate from the generated pool
+    predictions = [pred_beam[0] for pred_beam in predictions]
+    eval_configurations.insert(0, (predictions, False))
+
+    for prediction_list, reranked in eval_configurations:
+        file_name_root = compose_output_file_name(config, reranked=reranked)
+
+        # Save generated utterances along with their corresponding MRs into a CSV file
+        file_name = f'{file_name_root}.csv'
+        df_predictions = pd.DataFrame({'mr': test_set.get_mrs(raw=True), 'utt': prediction_list})
+        df_predictions.to_csv(os.path.join(predictions_dir, file_name), index=False, encoding='utf-8-sig')
+
+        # Save generated utterances in a text file (for reference-based metric evaluation)
+        file_name = f'{file_name_root}_utt_only.txt'
+        predictions_file = os.path.join(predictions_dir, file_name)
+        with open(predictions_file, 'w') as f_out:
+            for prediction in prediction_list:
+                f_out.write(prediction + '\n')
+
+        # Run the metrics script provided by the E2E NLG Challenge
+        os.system(metrics_script + ' ' + reference_file + ' ' + predictions_file)
 
 
-def compose_output_file_name(config):
+def compose_output_file_name(config, reranked=False):
     if config.num_beams > 1:
         inference_method_suffix = '_beam_search_'
-        if config.semantic_reranking:
+        if reranked:
             inference_method_suffix += 'reranked_'
         inference_method_suffix += str(config.length_penalty)
     elif config.do_sample and config.top_p < 1.0:
         inference_method_suffix = '_nucleus_sampling_'
-        if config.semantic_reranking:
+        if reranked:
             inference_method_suffix += 'reranked_'
         inference_method_suffix += str(config.top_p)
     elif config.do_sample and config.top_k > 0:
         inference_method_suffix = '_top_k_sampling_'
-        if config.semantic_reranking:
+        if reranked:
             inference_method_suffix += 'reranked_'
         inference_method_suffix += str(config.top_k)
     else:
