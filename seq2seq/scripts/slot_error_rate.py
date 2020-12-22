78cb76b4a6ec91fa054c946e8bf14dd0c20799aa
Jurik Juraska <juraska.juraj@gmail.com>
2020-12-21 22:24:24 -0800

    [slot-error-rate] Improves the exact-matching SER evaluation script.

--- seq2seq/scripts/slot_error_rate.py
+++ seq2seq/scripts/slot_error_rate.py
@@ -10,36 +10,21 @@ def calculate_slot_error_rate(data_dir, predictions_file, dataset_class, slot_le
     Slot realizations are only identified as exact slot value matches in utterances.
     """
     error_flags = []
-    slot_sep = ' | '
-    val_sep = ' = '
 
     # Load the input MRs and output utterances
     df_data = pd.read_csv(os.path.join(data_dir, predictions_file), header=0)
-    mrs_processed = dataset_class.preprocess_mrs(df_data.iloc[:, 0].to_list())
+    mrs_raw = df_data.iloc[:, 0].to_list()
+    mrs_processed = dataset_class.preprocess_mrs(mrs_raw, as_lists=True, lowercase=False, convert_slot_names=True)
     predictions = df_data.iloc[:, 1].to_list()
 
-    for mr, utt in zip(mrs_processed, predictions):
-        values = []
-
-        for slot_and_value_str in mr.split(slot_sep):
-            # Ignore slots with no values
-            if val_sep not in slot_and_value_str:
-                continue
-
-            # Parse the slot name and value
-            slot_and_value = slot_and_value_str.split(val_sep)
-            slot_name = slot_and_value[0].strip()
-            value = slot_and_value[1].strip()
-
-            # Ignore abstract slots and non-specific values
-            if slot_name in ['topic', 'intent'] or value in ['?', 'none']:
-                continue
-
-            values.append(value)
+    for mr_as_list, utt in zip(mrs_processed, predictions):
+        # Ignore abstract slots and non-specific values
+        mr_as_list = [(slot, value) for slot, value in mr_as_list
+                      if slot != '<|da|>' and value not in ['', '?', 'none']]
 
         # Determine if the utterance has any errors (i.e., missing slot mentions)
         utt_lowercased = utt.lower()
-        has_error = any(value.lower() not in utt_lowercased for value in values)
+        has_error = any(value.lower() not in utt_lowercased for value in map(lambda x: x[1], mr_as_list))
         error_flags.append(1 if has_error else 0)
 
     # Save the utterances along with their slot error indications to a CSV file
@@ -57,17 +42,19 @@ def calculate_slot_error_rate(data_dir, predictions_file, dataset_class, slot_le
     if verbose:
         print(f'>> Slot error rate: {round(100 * ser, 2)}%')
     else:
-        print(round(100 * ser, 2))
+        print(f'{round(100 * ser, 2)}%')
 
     return ser
 
 
 def main():
-    input_dir = os.path.join('..', 'predictions', 'multiwoz', 'finetuned_verbalized_slots', 'bart-base_lr_1e-5_bs_32_wus_500_run3')
-    predictions_file = 'epoch_16_step_1749_beam_search_1.0.csv'
+    # input_dir = os.path.join('..', 'predictions', 'multiwoz', 'finetuned_verbalized_slots', 'bart-base_lr_1e-5_bs_32_wus_500_run3')
+    # predictions_file = 'epoch_16_step_1749_beam_search_1.0.csv'
+    input_dir = os.path.join('..', 'data', 'multiwoz')
+    predictions_file = 'test.csv'
     dataset_class = MultiWOZDataset
 
-    calculate_slot_error_rate(input_dir, predictions_file, dataset_class)
+    calculate_slot_error_rate(input_dir, predictions_file, dataset_class, slot_level=False, verbose=False)
 
 
 if __name__ == '__main__':
