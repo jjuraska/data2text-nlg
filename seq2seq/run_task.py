95ec89fe597f6bed17699889bf8d9aaf1610a6cd
Jurik Juraska <juraska.juraj@gmail.com>
2021-04-08 18:49:27 -0700

    [NLG-87] Implements beam search decoding (for now working with batch size 1 only).

--- seq2seq/run_task.py
+++ seq2seq/run_task.py
@@ -7,9 +7,11 @@ import os
 import pickle
 import sys
 import torch
+from torch.nn import functional as F
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from transformers import AdamW, get_linear_schedule_with_warmup
+from transformers.generation_beam_search import BeamSearchScorer
 
 from seq2seq.data_loader import (
     E2EDataset, E2ECleanedDataset,
@@ -320,13 +322,24 @@ def generate_and_decode(config, data_loader, tokenizer, model, is_enc_dec, is_va
                                      **model_specific_args)
         else:
             if config.semantic_decoding:
-                num_seqs_per_input = 1
-                outputs, attn_weights, slot_error_list = semantic_decoding(input_tensor,
-                                                                           batch['slot_spans'],
-                                                                           tokenizer,
-                                                                           model,
-                                                                           max_length=config.max_seq_length,
-                                                                           device=device)
+                if config.num_beams > 1:
+                    num_seqs_per_input = config.num_return_sequences
+                    outputs, attn_weights, slot_error_list = semantic_decoding_beam_search(
+                        input_tensor,
+                        batch['slot_spans'],
+                        tokenizer,
+                        model,
+                        max_length=config.max_seq_length,
+                        num_beams=config.num_beams,
+                        device=device)
+                else:
+                    num_seqs_per_input = 1
+                    outputs, attn_weights, slot_error_list = semantic_decoding(input_tensor,
+                                                                               batch['slot_spans'],
+                                                                               tokenizer,
+                                                                               model,
+                                                                               max_length=config.max_seq_length,
+                                                                               device=device)
 
                 slot_errors.extend(slot_error_list)
 
@@ -459,6 +472,151 @@ def semantic_decoding(input_ids, slot_spans, tokenizer, model, max_length=128, d
     # print()
 
     return decoder_input_ids, attention_weights, slot_errors
+
+
+def semantic_decoding_beam_search(input_ids, slot_spans, tokenizer, model, max_length=128, num_beams=1,
+                                  length_penalty=1.0, early_stopping=False, device='cpu'):
+    """Performs a semantically guided inference from a structured MR input using beam search."""
+    outputs = None
+    attention_weights = {}
+
+    # Initialize the decoder's input sequence with the corresponding token
+    decoder_input_ids = torch.tensor([[model.config.decoder_start_token_id]], dtype=torch.long).to(device)
+
+    # Run the input sequence through the encoder and get the encoded input sequence
+    encoder = model.get_encoder()
+    encoded_sequence = encoder(input_ids, output_attentions=True)
+
+    # Determine the indices of special tokens in the input sequence
+    # special_tokens = [tok for tok in [tokenizer.bos_token_id, tokenizer.eos_token_id] if tok is not None]
+    # special_token_idxs = np.nonzero(input_ids.detach().cpu().numpy()[:, :, np.newaxis] == special_tokens)[:-1]
+
+    # DEBUG
+    # print('>> Indices of special tokens:')
+    # print(special_token_idxs)
+    # print()
+
+    # Save the encoder's self-attention weights
+    attention_weights['enc_attn_weights'] = [weight_tensor.squeeze().tolist() for weight_tensor in encoded_sequence.attentions]
+
+    batch_size = decoder_input_ids.shape[0]
+
+    beam_scorer = BeamSearchScorer(
+        batch_size=batch_size,
+        max_length=max_length,
+        num_beams=num_beams,
+        device=device,
+        length_penalty=length_penalty,
+        do_early_stopping=early_stopping,
+        num_beam_hyps_to_keep=num_beams,
+    )
+
+    decoder_input_ids, encoded_sequence = expand_inputs_for_beam_search(
+        decoder_input_ids, encoded_sequence, expand_size=num_beams)
+
+    cur_len = decoder_input_ids.shape[-1]
+
+    beam_scores = torch.zeros((batch_size, num_beams), dtype=torch.float, device=decoder_input_ids.device)
+    beam_scores[:, 1:] = -1e9
+    beam_scores = beam_scores.view((batch_size * num_beams,))
+
+    for step in range(max_length):
+        # Reuse the encoded inputs, and pass the sequence generated so far as inputs to the decoder
+        outputs = model(None, encoder_outputs=encoded_sequence, decoder_input_ids=decoder_input_ids,
+                        output_attentions=True, return_dict=True)
+
+        logits = outputs.logits
+
+        next_token_scores = F.log_softmax(logits[:, -1, :], dim=-1)    # (batch_size * num_beams, vocab_size)
+        next_token_scores = next_token_scores + beam_scores[:, None].expand_as(next_token_scores)
+
+        # Reshape for beam search
+        vocab_size = next_token_scores.shape[-1]
+        next_token_scores = next_token_scores.view(batch_size, num_beams * vocab_size)
+
+        # Keep (2 * num_beams) partial sequences with the highest scores
+        next_token_scores, next_tokens = torch.topk(next_token_scores, 2 * num_beams, dim=1, largest=True, sorted=True)
+
+        next_beam_idxs = next_tokens // vocab_size
+        next_token_ids = next_tokens % vocab_size
+
+        beam_outputs = beam_scorer.process(
+            decoder_input_ids,
+            next_token_scores,
+            next_token_ids,
+            next_beam_idxs,
+            pad_token_id=tokenizer.pad_token_id,
+            eos_token_id=tokenizer.eos_token_id,
+        )
+        beam_scores = beam_outputs['next_beam_scores']
+        beam_next_tokens = beam_outputs['next_beam_tokens']
+        beam_idx = beam_outputs['next_beam_indices']
+
+        # next_decoder_input_ids = select_next_token(
+        #     logits, outputs.cross_attentions, slot_spans, tokenizer.eos_token_id, special_token_idxs)
+
+        # Append the current output token's ID to the sequence generated so far
+        decoder_input_ids = torch.cat([decoder_input_ids[beam_idx, :], beam_next_tokens.unsqueeze(-1)], dim=-1)
+
+        # DEBUG
+        # for i in range(len(outputs.cross_attentions)):
+        #     print(outputs.cross_attentions[i].size())
+        # print()
+
+        cur_len = cur_len + 1
+
+        if beam_scorer.is_done:
+            break
+
+    sequence_outputs = beam_scorer.finalize(decoder_input_ids,
+                                            beam_scores,
+                                            next_token_ids,
+                                            next_beam_idxs,
+                                            pad_token_id=tokenizer.pad_token_id,
+                                            eos_token_id=tokenizer.eos_token_id)
+
+    if outputs:
+        # Save the decoder's self- and cross-attention weights; shape = (num_layers, batch_size, num_heads, sequence_length, sequence_length)
+        attention_weights['dec_attn_weights'] = [weight_tensor.squeeze().tolist()
+                                                 for weight_tensor in outputs.decoder_attentions]
+        attention_weights['cross_attn_weights'] = [weight_tensor.squeeze().tolist()
+                                                   for weight_tensor in outputs.cross_attentions]
+
+    # DEBUG
+    # print('>> Slot mentions:')
+    # print(slot_spans)
+    # print()
+
+    # slot_errors = evaluate_slot_mentions(slot_spans)
+    slot_errors = []
+
+    # DEBUG
+    # print('>> Slot errors:')
+    # print(slot_errors)
+    # print()
+
+    return sequence_outputs['sequences'], attention_weights, slot_errors
+
+
+def expand_inputs_for_beam_search(input_ids, enc_outputs, attention_mask=None, expand_size=1, **model_kwargs):
+    """Borrowed from Huggingface's transformers library."""
+    
+    expanded_indices = torch.arange(input_ids.shape[0]).view(-1, 1).repeat(1, expand_size).view(-1).to(input_ids.device)
+    input_ids = input_ids.index_select(0, expanded_indices)
+
+    # if 'token_type_ids' in model_kwargs:
+    #     token_type_ids = model_kwargs['token_type_ids']
+    #     model_kwargs['token_type_ids'] = token_type_ids.index_select(0, expanded_indices)
+
+    # if attention_mask is not None:
+    #     model_kwargs['attention_mask'] = attention_mask.index_select(0, expanded_indices)
+
+    enc_outputs['last_hidden_state'] = enc_outputs.last_hidden_state.index_select(
+        0, expanded_indices.to(enc_outputs.last_hidden_state.device)
+    )
+    # model_kwargs['encoder_outputs'] = enc_outputs
+
+    return input_ids, enc_outputs
 
 
 def evaluate_slot_mentions(slot_mentions_batch):
@@ -881,7 +1039,7 @@ def main():
         # input_str = "name[The Wrestlers], eatType[pub], food[Japanese], priceRange[£20-25], area[riverside], familyFriendly[yes], near[Raja Indian Cuisine]"
 
         # Video games (ViGGO)
-        # input_str = "inform(name[Tomb Raider: The Last Revelation], release_year[1999], esrb[T (for Teen)], genres[action-adventure, puzzle, shooter], platforms[PlayStation, PC], available_on_steam[yes], has_linux_release[no], has_mac_release[yes])"
+        input_str = "inform(name[Tomb Raider: The Last Revelation], release_year[1999], esrb[T (for Teen)], genres[action-adventure, puzzle, shooter], platforms[PlayStation, PC], available_on_steam[yes], has_linux_release[no], has_mac_release[yes])"
         # input_str = "inform(name[Assassin's Creed Chronicles: India], release_year[2016], genres[action-adventure, platformer], player_perspective[side view], has_multiplayer[no])"
         # input_str = "recommend(name[Crysis], has_multiplayer[yes], platforms[Xbox])"
         # input_str = "request_explanation(esrb[E (for Everyone)], rating[good], genres[adventure, platformer, puzzle])"
@@ -895,7 +1053,7 @@ def main():
         # input_str = "inform(name[F1 2014], release_year[2014], rating[average], genres[driving/racing, simulation, sport])"
         # input_str = "suggest(name[The Sims], platforms[PC], available_on_steam[no])"
         # input_str = "request_attribute(developer[])"
-        input_str = "recommend(name[F1 2014], genres[driving/racing, simulation, sport], platforms[PC])"
+        # input_str = "recommend(name[F1 2014], genres[driving/racing, simulation, sport], platforms[PC])"
 
         generate_from_input(TestConfig(config), input_str, dataset_class, device=device)
 
